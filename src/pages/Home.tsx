40e209a49c71a0f8869bd084329b406ddfe2abf7
Natan Falconi <nfalconi65@gmail.com>
2021-07-05 16:37:03 -0300

    AuthContext

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useHistory } from 'react-router-dom'
-
-import { auth, firebase } from '../services/firebase'
+// import { auth, firebase } from '../services/firebase'
+import { useContext } from 'react'
+import { AuthContext } from '../App'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -11,46 +12,46 @@ import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
 
-export function Home(){
-    
-    const history = useHistory()
+export function Home() {
 
-    function handlerCreateRoom (){
-        const provider = new firebase.auth.GoogleAuthProvider();
+  const history = useHistory()
 
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result)
- 
-            history.push('/rooms/new')
-        })
+  const { user, signInWithGoogle } = useContext(AuthContext)
+
+  async function handlerCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
     }
 
-    return(
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração" />
-                <strong>Toda pergunta tem <br /> uma resposta</strong>
-                <p>Aprenda e compartilhe conhecimentos <br /> com outras pessoas</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="letmeask" />
-                    <button onClick={handlerCreateRoom} className="create-room">
-                        <img src={googleIconImg} alt="logo do Google" />
+    history.push('/rooms/new')
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração" />
+        <strong>Toda pergunta tem <br /> uma resposta</strong>
+        <p>Aprenda e compartilhe conhecimentos <br /> com outras pessoas</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="letmeask" />
+          <button onClick={handlerCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="logo do Google" />
                         Crie sua sala com o google
                     </button>
-                    <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input 
-                        type="text" 
-                        placeholder="Digite o código da sala" 
-                        />
-                        <Button type="submit">
-                            Entrar na sala
-                        </Button>
-                    </form>
-                </div>
-            </main>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  )
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../App'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -7,32 +9,35 @@ import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
 
-export function NewRoom(){
+export function NewRoom() {
 
-    return(
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração" />
-                <strong>Toda pergunta tem <br /> uma resposta</strong>
-                <p>Aprenda e compartilhe conhecimentos <br /> com outras pessoas</p>
-            </aside>
-            <main>
+  const { user, signInWithGoogle } = useContext(AuthContext)
 
-                <div className="main-content">
-                    <img src={logoImg} alt="letmeask" />
-                    <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input 
-                        type="text" 
-                        placeholder="Nome da Sala" 
-                        />
-                        <Button type="submit">
-                            Criar sala
-                        </Button>
-                    </form>
-                    <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link> </p>
-                </div>
-            </main>
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração" />
+        <strong>Toda pergunta tem <br /> uma resposta</strong>
+        <p>Aprenda e compartilhe conhecimentos <br /> com outras pessoas</p>
+      </aside>
+      <main>
+
+        <div className="main-content">
+          <img src={logoImg} alt="letmeask" />
+          <h1>{user?.name}</h1>
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da Sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link> </p>
         </div>
-    )
+      </main>
+    </div>
+  )
 }
